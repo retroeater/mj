bce44df98bbfb8dc89e28cbef7f278e955d34e64
retroeater <hirano@ryoei.net>
2020-07-20 05:23:39 +0900

    added glossary.

---
+++ glossary.js
@@ -0,0 +1,33 @@
+const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1kFcj_ojKUtSKjRVs7Mres7udPxdQY4zO8wpUG-ogtSc/edit#gid=0'
+
+google.charts.load('current', {'packages':['table']});
+google.charts.setOnLoadCallback(drawTable);
+
+function drawTable() {
+
+	const query = new google.visualization.Query(spreadsheet_url)
+	query.setQuery('SELECT A,B,C,D')
+	query.send(handleQueryResponse)
+
+	function handleQueryResponse(response) {
+		if(response.isError()) {
+			alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage())
+			return
+		}
+
+		var data = response.getDataTable()
+
+		const table = new google.visualization.Table(document.getElementById('myTable'));
+
+		const options = {
+			allowHtml: true,
+			width: '100%',
+			height: '100%'
+		}
+
+		// 必要列のみ表示
+		const view = new google.visualization.DataView(data)
+
+		table.draw(view, options);
+	}
+}
