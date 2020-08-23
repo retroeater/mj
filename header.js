ef5fd2d2c13c9024c688edcd05e167898c31cf87
retroeater <hirano@ryoei.net>
2020-08-23 09:53:34 +0900

    added Google Analytics.

--- header.js
+++ header.js
@@ -4,8 +4,8 @@ document.write(
 '<script>' +
 '  window.dataLayer = window.dataLayer || []' +
 '  function gtag(){dataLayer.push(arguments)}' +
-'  gtag("js", new Date())' +
-'  gtag("config", "UA-176078855-1")' +
+'  gtag(\'js\', new Date())' +
+'  gtag(\'config\', \'UA-176078855-1\')' +
 '</script>' +
 '<meta charset="utf-8" />' +
 '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />' +
