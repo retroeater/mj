db6c6810ad3f5824269202a3e051bda9ed54548b
retroeater <hirano@ryoei.net>
2020-10-20 09:18:48 +0900

    reversed leagues

--- leagues.js
+++ leagues.js
@@ -6,7 +6,7 @@ google.charts.setOnLoadCallback(drawChart)
 function drawChart() {
 
 	const query = new google.visualization.Query(spreadsheet_url)
-	query.setQuery('SELECT A,L,K,J,I,H,G,F,E,D,C,B,M,N,O')
+	query.setQuery('SELECT A,B,C,D,E,F,G,H,I,J,K,L,M,N,O')
 	query.send(handleQueryResponse)
 
 	function handleQueryResponse(response) {
@@ -18,31 +18,48 @@ function drawChart() {
 		const data = response.getDataTable()
 		
         const options = {
-			isStacked: true,
-			legend: {position: 'bottom'},
-			seriesType: 'bars',
+			animation: {
+				duration: 1000,
+				easing: 'out',
+				startup: true
+			},
+//			axisTitlePosition: 'out',
+			chartArea: {
+				left: 20,
+				top: 20,
+				width: '100%',
+				height: '80%'
+			},
 			colors: [
-				'#CCCCCC', // E
-				'#CCCCFF', // D3
-				'#CCDDFF', // D2
-				'#CCEEFF', // D1
-				'#CCFFFF', // C3
-				'#DDFFEE', // C2
-				'#EEFFDD', // C1
-				'#FFFFCC', // B2
-				'#FFEECC', // B1
-				'#FFDDCC', // A2
 				'#FFCCCC', // A1
+				'#FFDDCC', // A2
+				'#FFEECC', // B1
+				'#FFFFCC', // B2
+				'#EEFFDD', // C1
+				'#DDFFEE', // C2
+				'#CCFFFF', // C3
+				'#CCEEFF', // D1
+				'#CCDDFF', // D2
+				'#CCCCFF', // D3
+				'#CCCCCC', // E
 				'#CC0000', // 石立岳大
 				'#009900', // 西川淳
 				'#0000CC'  // 平野良栄
 			],
+			curveType:  'function',
+			interpolateNulls: true,
+			isStacked: true,
+			legend: {position: 'bottom'},
+			seriesType: 'bars',
 			series: {
 				11: {type:'line'},
 				12: {type:'line'},
 				13: {type:'line'},
 			},
-			interpolateNulls: true
+			vAxis: {
+				direction: -1,
+				textPosition: 'none'
+			}
         }
 
         const chart = new google.visualization.ColumnChart(document.getElementById('myChart'))
