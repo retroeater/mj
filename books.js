5afc6f2dde7bd49df504065a4335f08cb1890587
retroeater <hirano@ryoei.net>
2021-01-06 07:40:54 +0900

    added kindle unlimited links to books.

--- books.js
+++ books.js
@@ -6,7 +6,7 @@ google.charts.setOnLoadCallback(drawTable);
 function drawTable() {
 
 	const query = new google.visualization.Query(spreadsheet_url)
-	query.setQuery('SELECT A,B,C,D,E,F,G,H')
+	query.setQuery('SELECT A,B,C,D,E,F,G,H,I')
 	query.send(handleQueryResponse)
 
 	function handleQueryResponse(response) {
@@ -19,6 +19,36 @@ function drawTable() {
 
 		const table = new google.visualization.Table(document.getElementById('myTable'));
 
+		for(let i = 0; i < data.getNumberOfRows(); i++) {
+
+				// 著者名フォーマット
+				let formattedAuthor = getFormattedAuthor(data,i)
+
+				// 商品画像フォーマット
+				let formattedProductImage = getFormattedProductImage(data,i)
+
+				// 書籍名フォーマット
+				let formattedTitle = getFormattedTitle(data,i)
+
+				// 出版日フォーマット
+				let formattedPublishedDate = getFormattedPublishedDate(data,i)
+
+				data.setValue(i, 0, formattedAuthor)
+				data.setValue(i, 3, formattedProductImage)
+				data.setValue(i, 5, formattedTitle)
+				data.setValue(i, 7, formattedPublishedDate)
+		}
+
+
+
+
+
+
+
+
+
+/*
+
 		// 著者名フォーマット
 		const authorFormatter = new google.visualization.PatternFormat('{0} （{1}）<br>{2}')
 		authorFormatter.format(data,[0,1,2],0)
@@ -28,9 +58,10 @@ function drawTable() {
 		imageFormatter.format(data,[3,5,4],3)
 
 		// 書籍名フォーマット
-		const titleFormatter = new google.visualization.PatternFormat('{0}<br>{1}')
-		titleFormatter.format(data,[5,6],5)
+		const titleFormatter = new google.visualization.PatternFormat('{0}<br>{1}<br><a href="https://www.amazon.co.jp/dp/{2}"><img alt="kindle unlimited" height="18" src="img/ku-logo-orange-black._CB485944605_.png"></a>')
+		titleFormatter.format(data,[5,6,8],5)
 
+*/
 		// 必要列のみ表示
 		const view = new google.visualization.DataView(data)
 		view.setColumns([0,3,5,7])
@@ -46,4 +77,58 @@ function drawTable() {
 
 		table.draw(view, options);
 	}
+}
+
+function getFormattedAuthor(data,row_index) {
+
+	const author_ja_kanji = data.getValue(row_index,0)
+	const author_ja_kana  = data.getValue(row_index,1)
+	const author_en       = data.getValue(row_index,2)
+
+	let formattedAuthor = ""
+
+	formattedAuthor = author_ja_kanji + '（' + author_ja_kana + '）<br>' + author_en
+
+	return formattedAuthor
+}
+
+function getFormattedProductImage(data,row_index) {
+
+	const product_id    = data.getValue(row_index,3)
+	const product_image = data.getValue(row_index,4)
+	const title_ja      = data.getValue(row_index,5)
+
+	let formattedProductImage = ""
+
+	formattedProductImage = '<a href="https://www.amazon.co.jp/dp/' + product_id + '" target="_blank"><img alt="' + title_ja + '" src="' + product_image + '" /></a>'
+
+	return formattedProductImage
+}
+
+function getFormattedTitle(data,row_index) {
+
+	const title_ja  = data.getValue(row_index,5)
+	const title_en  = data.getValue(row_index,6)
+	const kindle_id = data.getValue(row_index,8)
+
+	let formattedTitle = ""
+
+	formattedTitle = title_ja + '<br>' + title_en
+
+	if(kindle_id) {
+		formattedTitle = formattedTitle + '<br><a href="https://www.amazon.co.jp/dp/' + kindle_id + '/" target="_blank"><img alt="kindle unlimited" height="18" src="img/ku-logo-orange-black._CB485944605_.png"></a>'
+	}
+
+	return formattedTitle
+}
+
+function getFormattedPublishedDate(data,row_index) {
+
+	const published_date = data.getValue(row_index,7)
+
+	let formattedPublishedDate = ""
+
+	formattedPublishedDate = published_date
+
+	return formattedPublishedDate
 }
