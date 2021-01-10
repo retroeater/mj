9821a1bf96d8891c6fb2f2fe5ff659712877a204
retroeater <hirano@ryoei.net>
2021-01-11 06:53:07 +0900

    Refer to Amazon by IMPORTXML

--- books.js
+++ books.js
@@ -1,4 +1,4 @@
-const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1h4-DhmvaBJzfkA61mTKkz4mMuICGliuzglakql5TeP0/edit?sheet=books'
+const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1h4-DhmvaBJzfkA61mTKkz4mMuICGliuzglakql5TeP0/edit?sheet=books&headers=1'
 
 google.charts.load('current', {'packages':['table']});
 google.charts.setOnLoadCallback(drawTable);
@@ -6,7 +6,7 @@ google.charts.setOnLoadCallback(drawTable);
 function drawTable() {
 
 	const query = new google.visualization.Query(spreadsheet_url)
-	query.setQuery('SELECT A,B,C,D,E,F,G,H,I')
+	query.setQuery('SELECT B,C,D,F,G,I,J,M,O,P')
 	query.send(handleQueryResponse)
 
 	function handleQueryResponse(response) {
@@ -39,29 +39,6 @@ function drawTable() {
 				data.setValue(i, 7, formattedPublishedDate)
 		}
 
-
-
-
-
-
-
-
-
-/*
-
-		// 著者名フォーマット
-		const authorFormatter = new google.visualization.PatternFormat('{0} （{1}）<br>{2}')
-		authorFormatter.format(data,[0,1,2],0)
-
-		// 商品画像フォーマット
-		const imageFormatter = new google.visualization.PatternFormat('<a href="https://www.amazon.co.jp/dp/{0}/" target="_blank"><img alt="{1}" src="{2}" /></a>')
-		imageFormatter.format(data,[3,5,4],3)
-
-		// 書籍名フォーマット
-		const titleFormatter = new google.visualization.PatternFormat('{0}<br>{1}<br><a href="https://www.amazon.co.jp/dp/{2}"><img alt="kindle unlimited" height="18" src="img/ku-logo-orange-black._CB485944605_.png"></a>')
-		titleFormatter.format(data,[5,6,8],5)
-
-*/
 		// 必要列のみ表示
 		const view = new google.visualization.DataView(data)
 		view.setColumns([0,3,5,7])
@@ -94,29 +71,30 @@ function getFormattedAuthor(data,row_index) {
 
 function getFormattedProductImage(data,row_index) {
 
-	const product_id    = data.getValue(row_index,3)
+	const book_url      = data.getValue(row_index,3)
 	const product_image = data.getValue(row_index,4)
 	const title_ja      = data.getValue(row_index,5)
 
 	let formattedProductImage = ""
 
-	formattedProductImage = '<a href="https://www.amazon.co.jp/dp/' + product_id + '" target="_blank"><img alt="' + title_ja + '" src="' + product_image + '" /></a>'
+	formattedProductImage = '<a href="' + book_url + '" target="_blank"><img alt="' + title_ja + '" src="' + product_image + '" /></a>'
 
 	return formattedProductImage
 }
 
 function getFormattedTitle(data,row_index) {
 
-	const title_ja  = data.getValue(row_index,5)
-	const title_en  = data.getValue(row_index,6)
-	const kindle_id = data.getValue(row_index,8)
+	const title_ja     = data.getValue(row_index,5)
+	const title_en     = data.getValue(row_index,6)
+	const kindle_url   = data.getValue(row_index,8)
+	const is_unlimited = data.getValue(row_index,9)
 
 	let formattedTitle = ""
 
 	formattedTitle = title_ja + '<br>' + title_en
 
-	if(kindle_id) {
-		formattedTitle = formattedTitle + '<br><a href="https://www.amazon.co.jp/dp/' + kindle_id + '/" target="_blank"><img alt="kindle unlimited" height="18" src="img/ku-logo-orange-black._CB485944605_.png"></a>'
+	if(is_unlimited) {
+		formattedTitle = formattedTitle + '<br><a href="' + kindle_url + '/" target="_blank"><img alt="kindle unlimited" height="18" src="img/ku-logo-orange-black._CB485944605_.png"></a>'
 	}
 
 	return formattedTitle
