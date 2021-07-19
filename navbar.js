047641120d6421b2d1809a45cc9913482fdeaa1e
retroeater <hirano@ryoei.net>
2021-07-20 01:26:54 +0900

    added ranking_wrc.

--- navbar.js
+++ navbar.js
@@ -56,6 +56,7 @@ document.write(
 					'<div class="dropdown-menu" aria-labelledby="navbarDropdown">' +
 						'<a class="dropdown-item" href="ranking_houou.html">鳳凰戦</a>' +
 						'<a class="dropdown-item" href="ranking_ouka.html">女流桜花</a>' +
+						'<a class="dropdown-item" href="ranking_wrc.html">JPML WRC</a>' +
 						'<a class="dropdown-item" href="ranking_ron2.html">ロン2</a>' +
 						'<a class="dropdown-item" href="ranking_youtube.html">YouTube</a>' +
 						'<a class="dropdown-item" href="ranking_misc.html">その他</a>' +
