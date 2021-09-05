b1bf744b748c79f0ec1d897e37bce0ba875a18d5
retroeater <hirano@ryoei.net>
2021-09-06 01:13:15 +0900

    renamed sheets.

--- judan_38.js
+++ judan_38.js
@@ -1,4 +1,4 @@
-const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1h4-DhmvaBJzfkA61mTKkz4mMuICGliuzglakql5TeP0/edit?sheet=judan&headers=1'
+const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1h4-DhmvaBJzfkA61mTKkz4mMuICGliuzglakql5TeP0/edit?sheet=十段&headers=1'
 
 const params = (new URL(document.location)).searchParams
 let search_name = params.get('name')
