941a726e58b0b87f99e75bc7b7010cdddc4abbb2
retroeater <hirano@ryoei.net>
2022-04-11 01:34:58 +0900

    fixed the navigation bar.

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 document.write(
-'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">' +
+'<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">' +
 	'<div class="container-fluid">' +
 		'<a class="navbar-brand" href="index.html">M</a>' +
 		'<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +
