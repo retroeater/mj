71bf4f26c53299245440eb5b0a24669722c77e5d
retroeater <hirano@ryoei.net>
2022-04-11 03:13:13 +0900

    upgraded from Bootstrap 5.0.1 to 5.1.3

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ document.write(
 '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />' +
 '<link rel="shortcut icon" href="favicon.ico" />' +
 '<!-- Bootstrap -->' +
-'<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">' +
+'<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">' +
 '<!-- Google Charts -->' +
 '<script src="https://www.gstatic.com/charts/loader.js"></script>' +
 '<!-- mj -->' +
--- navbar.js
+++ navbar.js
@@ -102,5 +102,5 @@ document.write(
 '</nav>' +
 
 '<!-- Bootstrap -- Bootstrap Bundle JS -->' +
-'<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>'
+'<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>'
 )
