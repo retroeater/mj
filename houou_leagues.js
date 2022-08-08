0f4c1832b39fdd5f70f912cb11cd0a0e4a0d0d90
retroeater <hirano@ryoei.net>
2022-08-08 09:15:10 +0000

    removed a logic to copy 39H2 to 39H1 for A1/A2

--- houou_leagues.js
+++ houou_leagues.js
@@ -246,7 +246,6 @@ function drawChart() {
 			leagues_36_1[1] = leagues_36_2[1]
 			leagues_37_1[1] = leagues_37_2[1]
 			leagues_38_1[1] = leagues_38_2[1]
-			leagues_39_1[1] = leagues_39_2[1]
 
 			// 前期A2リーグ人数補完
 			leagues_18_1[2] = leagues_18_2[2]
@@ -270,7 +269,6 @@ function drawChart() {
 			leagues_36_1[2] = leagues_36_2[2]
 			leagues_37_1[2] = leagues_37_2[2]
 			leagues_38_1[2] = leagues_38_2[2]
-			leagues_39_1[2] = leagues_39_2[2]
 		}
 
 		let chartData = new google.visualization.DataTable()
