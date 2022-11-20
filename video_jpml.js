38d8ba00c3796cfe5d0527f21334786daae77e10
retroeater <hirano@ryoei.net>
2022-11-20 10:10:14 +0000

    switched from video to 動画

--- video_jpml.js
+++ video_jpml.js
@@ -1,4 +1,4 @@
-const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1h4-DhmvaBJzfkA61mTKkz4mMuICGliuzglakql5TeP0/edit?sheet=video&headers=1'
+const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1h4-DhmvaBJzfkA61mTKkz4mMuICGliuzglakql5TeP0/edit?sheet=動画&headers=1'
 
 const params = (new URL(document.location)).searchParams
 let search_name = params.get('name')
