848311fbe6f7f44c4bb20483cf2564de243ceeb6
retroeater <hirano@ryoei.net>
2023-08-14 08:33:28 +0000

    removed menber-only videos

--- video_live.js
+++ video_live.js
@@ -1,4 +1,4 @@
-const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1h4-DhmvaBJzfkA61mTKkz4mMuICGliuzglakql5TeP0/edit?sheet=live&headers=1'
+const spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1h4-DhmvaBJzfkA61mTKkz4mMuICGliuzglakql5TeP0/edit?sheet=対局&headers=1'
 
 const params = (new URL(document.location)).searchParams
 let search_name = params.get('name')
