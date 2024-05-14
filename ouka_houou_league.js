438773f634d49b34c6dba60a7b14ea8b91ca1028
retroeater <hirano@ryoei.net>
2024-05-14 15:22:07 +0000

    corrected from Houou 41H2 to 41H1

--- ouka_houou_league.js
+++ ouka_houou_league.js
@@ -85,7 +85,7 @@ function drawChart() {
 				maxSize: 50,
 				minSize: 10
 			},
-			title: '第19期女流桜花✕第41期後期鳳凰リーグ',
+			title: '第19期女流桜花✕第41期前期鳳凰リーグ',
 			titlePosition: 'in',
 			tooltip: {
 				trigger:  'none'
