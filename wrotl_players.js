e14ef1db63715fb9543d3f5bde3855d22fc08ac3
retroeater <hirano@ryoei.net>
2024-07-17 08:34:27 +0000

    added playerNameEn

--- wrotl_players.js
+++ wrotl_players.js
@@ -4,7 +4,7 @@ const params = (new URL(document.location)).searchParams
 let search_name = params.get('name')
 let search_tag = params.get('tag')
 
-let queryStatement = 'SELECT A,B,C,D,E,F,G,H,I WHERE J = "Y"'
+let queryStatement = 'SELECT A,B,C,D,E,F,G,H,I,J WHERE K = "Y"'
 
 if(search_name) {
 	queryStatement += ' AND A = "' + search_name + '"'
@@ -24,14 +24,15 @@ function drawDashboard() {
 	query.send(handleQueryResponse)
 
 	let playerId		// A 選手名
-	let playerName		// B 選手名Ⓟなし
-	let playerType		// C 選手区分
-	let playerOrg		// D 所属
-	let teamId			// E チームID
-	let teamType		// F チーム区分
-	let teamName		// G チーム名
-	let twitterId		// H X ID
-	let twitterImageUrl	// I X画像URL
+	let playerNameJa	// B 選手名Ⓟなし（日本語）
+	let playerNameEn	// C 選手名Ⓟなし（英語）
+	let playerType		// D 選手区分
+	let playerOrg		// E 所属
+	let teamId			// F チームID
+	let teamType		// G チーム区分
+	let teamName		// H チーム名
+	let twitterId		// I X ID
+	let twitterImageUrl	// J X画像URL
 
 	function handleQueryResponse(response) {
 
@@ -49,17 +50,18 @@ function drawDashboard() {
 		for(let i = 0; i < data.getNumberOfRows(); i++) {
 
 			playerId = data.getValue(i,0)
-			playerName = data.getValue(i,1)
-			playerType = data.getValue(i,2)
-			playerOrg = data.getValue(i,3)
-			teamId = data.getValue(i,4)
-			teamType = data.getValue(i,5)
-			teamName = data.getValue(i,6)
-			twitterId = data.getValue(i,7)
-			twitterImageUrl = data.getValue(i,8)
+			playerNameJa = data.getValue(i,1)
+			playerNameEn = data.getValue(i,2)
+			playerType = data.getValue(i,3)
+			playerOrg = data.getValue(i,4)
+			teamId = data.getValue(i,5)
+			teamType = data.getValue(i,6)
+			teamName = data.getValue(i,7)
+			twitterId = data.getValue(i,8)
+			twitterImageUrl = data.getValue(i,9)
 
-			let formattedImage = getFormattedImage(playerName,twitterId,twitterImageUrl)
-			let formattedProfile = getFormattedProfile(playerId,playerType,playerOrg,teamName)
+			let formattedImage = getFormattedImage(playerNameJa,twitterId,twitterImageUrl)
+			let formattedProfile = getFormattedProfile(playerId,playerNameEn,playerType,playerOrg,teamName)
 
 			chartData.addRows([
 				[
@@ -106,27 +108,27 @@ function drawDashboard() {
 	}
 }
 
-function getFormattedImage(playerName,twitterId,twitterImageUrl) {
+function getFormattedImage(playerNameJa,twitterId,twitterImageUrl) {
 
 	let formattedImage
 	const linkIcon = 'img/125_arr_hoso.png'
 
 	let twitterUrl = getTwitterUrl(twitterId)
 
-	formattedImage = '<a href="' + twitterUrl + '" target="_blank"><img alt="' + playerName + '" class="x" loading="lazy" src="' + twitterImageUrl + '" onError="this.onerror=null;this.src=\'' + linkIcon + '\'" /></a>'
+	formattedImage = '<a href="' + twitterUrl + '" target="_blank"><img alt="' + playerNameJa + '" class="x" loading="lazy" src="' + twitterImageUrl + '" onError="this.onerror=null;this.src=\'' + linkIcon + '\'" /></a>'
 
 	return formattedImage
 }
 
-function getFormattedProfile(playerId,playerType,playerOrg,teamName) {
+function getFormattedProfile(playerId,playerNameEn,playerType,playerOrg,teamName) {
 
 	let formattedProfile
 
 	if(playerOrg) {
-		formattedProfile = teamName + '<br>' + playerId + '/ FirstName LastName<br>' + playerOrg + 'orgEnglishNameHere'
+		formattedProfile = teamName + '<br>' + playerId + ' / ' + playerNameEn + '<br>' + playerOrg + 'orgEnglishNameHere'
 	}
 	else {
-		formattedProfile = teamName + '<br>' + playerId + '/ FirstName LastName'
+		formattedProfile = teamName + '<br>' + playerId + ' / ' + playerNameEn
 	}
 
 	return formattedProfile
