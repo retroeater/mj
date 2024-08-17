9794aa5396b02edbecfb2295bb62149a0e33d991
retroeater <hirano@ryoei.net>
2024-08-17 07:25:36 +0000

    removed Tokusho

--- navbar.js
+++ navbar.js
@@ -42,12 +42,6 @@ document.write(
 					'<a class="dropdown-item" href="wrc_results.html">成績詳細</a>' +
 					'</div>' +
 				'</li>' +
-				'<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">特別昇級<br>Tokusho</a>' +
-					'<div class="dropdown-menu" aria-labelledby="navbarDropdown">' +
-					'<a class="dropdown-item" href="tokusho_ranking.html?sheet=特昇">ランキング</a>' +
-					'<a class="dropdown-item" href="tokusho_results.html">成績詳細</a>' +
-					'</div>' +
-				'</li>' +
 				'<li class="nav-item"><a class="nav-link" href="saikyo_results.html">最強戦<br>Saikyo</a></li>' +
 				'<li class="nav-item"><a class="nav-link" href="wrotl_players.html">WROTL<br>2024</a></li>' +
 				'<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">動画<br>Video</a>' +
