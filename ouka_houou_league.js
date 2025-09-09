f7f5553868e5e6081b68839492fb3c1d779c79b0
retroeater <hirano@ryoei.net>
2025-09-09 15:01:26 +0000

    updated from Houou 42H1 to 42H2

--- ouka_houou_league.js
+++ ouka_houou_league.js
@@ -85,7 +85,7 @@ function drawChart() {
 				maxSize: 50,
 				minSize: 10
 			},
-			title: '第20期女流桜花✕第42期前期鳳凰リーグ',
+			title: '第20期女流桜花✕第42期後期鳳凰リーグ',
 			titlePosition: 'in',
 			tooltip: {
 				trigger:  'none'
