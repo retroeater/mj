062d12cbb52303546411f71ea37f5569f1698e9b
retroeater <hirano@ryoei.net>
2025-09-09 14:42:30 +0000

    updated from Houou 42H1 to 42H2

--- houou_league_by_class.js
+++ houou_league_by_class.js
@@ -83,7 +83,7 @@ function drawChart() {
 				maxSize: 50,
 				minSize: 10
 			},
-			title: 'リーグ✕期（42期前期時点）',
+			title: 'リーグ✕期（42期後期時点）',
 			titlePosition: 'in',
 			tooltip: {
 				trigger:  'none'
