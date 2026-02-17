de5217c8a6ceec3de84cf4d5ff31be88b7c63400
retroeater <hirano@ryoei.net>
2026-02-17 12:10:34 +0000

    updated to 42H2

--- houou_leagues.js
+++ houou_leagues.js
@@ -370,7 +370,8 @@ function drawChart() {
 			leagues_40_2,
 			leagues_41_1,
 			leagues_41_2,
-			leagues_42_1
+			leagues_42_1,
+			leagues_42_2
 		])
 
 		let leagueRanks = getLeagueRanks(data,chartData,search_name)
