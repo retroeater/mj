e0dd9619e53342d322dff99cf6f834cf1970f1d6
retroeater <hirano@ryoei.net>
2026-02-18 06:11:06 +0000

    added video > wayhome

--- navbar.js
+++ navbar.js
@@ -45,6 +45,7 @@ document.write(
 				'<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">動画<br>Video</a>' +
 					'<div class="dropdown-menu" aria-labelledby="navbarDropdown">' +
 						'<a class="dropdown-item" href="video_live.html">放送対局</a>' +
+						'<a class="dropdown-item" href="video_wayhome.html">帰り道</a>' +
 						'<a class="dropdown-item" href="video_mtsuku.html">Mつく</a>' +
 						'<a class="dropdown-item" href="video_en.html">English</a>' +
 					'</div>' +
